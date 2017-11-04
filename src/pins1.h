5edd0bcd44b0085cb373ce84b78694ba1a7a22ec
David <david.amison20@gmail.com>
2017-11-04 14:17:55 +0000

    Debugging

--- src/pins1.h
+++ src/pins1.h
@@ -7,20 +7,22 @@
  * @version 1.0 28/10/2017
  */
 
-#ifndef PIN1S_H
+#ifndef PINS1_H
 #define PINS1_H
 
 // Signals for main logic control
 #define LO    29
-#define SOE    28
+#define SOE    23
 #define SODS   27
 #define LAUNCH_MODE  11
+
+// Pins for control of the motor
+#define MOTOR_CW  24
+#define MOTOR_ACW  25
+#define MOTOR_IN  0
 
 // Connection between pi 1 and 2
 #define ALIVE   3
 
-// Pins for control of burn wire
-#define BURNWIRE  4
-
-#endif /* PINS_H */
+#endif /* PINS1_H */
 
--- src/pins2.h
+++ src/pins2.h
@@ -7,19 +7,14 @@
  * @version 1.0 28/10/2017
  */
 
-#ifndef PIN1S_H
-#define PINS1_H
+#ifndef PINS2_H
+#define PINS2_H
 
 // Signals for main logic control
 #define LO    29
-#define SOE    23
+#define SOE    28
 #define SODS   27
 #define LAUNCH_MODE  11
-
-// Pins for control of the motor
-#define MOTOR_CW  24
-#define MOTOR_ACW  25
-#define MOTOR_IN  0
 
 // Connection between pi 1 and 2
 #define ALIVE   3
@@ -27,5 +22,5 @@
 // Pins for control of burn wire
 #define BURNWIRE  4
 
-#endif /* PINS_H */
+#endif /* PINS2_H */
 
