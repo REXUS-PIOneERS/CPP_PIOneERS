e4c0941fff5dd215adc848947d504d0131e828b0
David <david.amison20@gmail.com>
2017-11-04 14:12:26 +0000

    Debugging UARTException

--- src/UART/UART.cpp
+++ src/UART/UART.cpp
@@ -34,7 +34,8 @@ void UART::setupUART() {
 	//Open the UART in non-blocking read/write mode
 	uart_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY);
 	if (uart_filestream == -1) {
-		throw UARTException("ERROR opening serial port");
+		//throw UARTException("ERROR opening serial port");
+		return;
 	}
 	speed_t f_baud;
 	switch (_baudrate) {
